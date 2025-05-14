ef52a39a5b4bbba56fdb340de0165cd6c748f908
rubicus3 <laba625@yandex.ru>
2025-05-15 01:38:18 +0300

    Add init test script

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,4 +1,10 @@
 CREATE TABLE public.keyboards (
-	id varchar GENERATED ALWAYS AS IDENTITY NOT NULL,
-	CONSTRAINT keyboards_pk PRIMARY KEY (id)
+	id serial primary key,
+    name varchar(100),
+    price real,
+    form_factor varchar(50),
+    manufacturer varchar(50),
+    description text,
+    characteristics JSON,
+    image_name varchar(50)
 );
